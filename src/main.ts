2f85de8d4ece82101857c37cd8fa6a543ae4c8d5
nicolassanchez1 <nicolassanchez115@hotmail.com>
2026-03-23 17:25:02 -0500

    feat: config linter and prettier

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  const port = process.env.PORT || 3003;
+  await app.listen(port);
+  console.log(`Auth Microservice is running on port: ${port}`);
 }
-bootstrap();
+void bootstrap();
